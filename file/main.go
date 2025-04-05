5f3fa04a709c37503ffe6dfbccfb95293217dffc
Sadtegy <shakiul017@gmail.com>
2025-04-05 12:37:11 +0600

    presting the MyEidList

---
+++ file/main.go
@@ -0,0 +1,27 @@
+// Creating my first todolist application by using go language
+// Basically it focus on eid day
+package main
+
+import "fmt"
+
+func main() {
+	cloth := "Buy new cloth for eid."
+	clean := "Clean & decorate the house."
+	food := "Prepare or order special Eid food."
+	wishes := "send Eid wishes to family & friends."
+	money := "Set aside money or gifts for Eidi."
+
+	todoItems := []string{cloth, clean, food, wishes, money}
+
+	fmt.Println("***Welcome to our Todolist***")
+	fmt.Println()
+	fmt.Println("list of todos")
+	fmt.Println()
+
+	for index, task := range todoItems {
+
+		fmt.Println(index, task) //after using is it well print the result ofc
+
+	}
+
+}
